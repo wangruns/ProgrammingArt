b6015205b079e86b330ea64f587de83030cac908
wangruns <1344886192@qq.com>
2017-08-29 18:46:38 +0800

    add  CopyListWithRandomPointer and so on

--- src/com/leetcode/wangruns/Leetcode.java
+++ src/com/leetcode/wangruns/Leetcode.java
@@ -78,6 +78,75 @@ public class Leetcode {
 		
 	}
 	
+	//13、copy-list-with-random-pointer[链表]
+	/**
+	 * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+	 * Return a deep copy of the list.
+	 * 
+	 * 新的链表和旧的链表，一种对应关系，联想到	map
+	 * 建立老节点和新节点的一一对应关系			map(old,new)
+	 * 根据老节点的关系，建立新节点的关系		map.get(old).next=map.get(old.next);
+	 */
+	
+	/**
+	 * Definition for singly-linked list with a random pointer.
+	 * class RandomListNode {
+	 *     int label;
+	 *     RandomListNode next, random;
+	 *     RandomListNode(int x) { this.label = x; }
+	 * };
+	 */
+	class RandomListNode{
+		int label;
+		RandomListNode next,random;
+		RandomListNode(int x){ this.label=x; }
+	}
+	//RandomListNode在list中
+	public RandomListNode copyRandomList1(RandomListNode head) {
+		if(head==null) return head;
+		HashMap<RandomListNode,RandomListNode> map=new HashMap<>();
+		//建立新节点和老节点的关系
+		RandomListNode p=head;
+		while(p!=null){
+			map.put(p, new RandomListNode(p.label));
+			p=p.next;
+		}
+		//更具老节点的关系，建立新节点的关系
+		p=head;
+		RandomListNode newNode=null;
+		while(p!=null){
+			newNode=map.get(p);
+			if(p.next!=null) newNode.next=map.get(p.next);
+			if(p.random!=null) newNode.random=map.get(p.random);
+			p=p.next;
+		}
+		return map.get(head);
+	}
+	
+	//RandomListNode可以不在list中
+	public RandomListNode copyRandomList(RandomListNode head) {
+        if(head==null) return head;
+        RandomListNode p=head;
+        HashMap<RandomListNode,RandomListNode> map=new HashMap<>();
+        //建立老节点和新节点的一一对应关系
+        while(p!=null){
+        	map.put(p, new RandomListNode(p.label));
+        	if(p.next!=null) map.put(p.next, new RandomListNode(p.next.label));
+        	if(p.random!=null) map.put(p.random, new RandomListNode(p.random.label));
+        	p=p.next;
+        }
+        //根据老节点的关系，建立新节点的关系
+        p=head;
+        RandomListNode newNode=null;
+        while(p!=null){
+        	newNode=map.get(p);
+        	if(p.next!=null) newNode.next=map.get(p.next);
+        	if(p.random!=null) newNode.random=map.get(p.random);
+        	p=p.next;
+        }
+        return map.get(head);
+    }
+	
 	//12、word-break-ii[动态规划]
 	/**
 	Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
