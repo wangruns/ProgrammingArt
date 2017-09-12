1556669f0524d462f33e6cb117213b14d0f142a0
wangruns <1344886192@qq.com>
2017-09-12 17:10:40 +0800

    add BestTimeToBuyAndSellStock and so on

--- src/com/leetcode/wangruns/Leetcode.java
+++ src/com/leetcode/wangruns/Leetcode.java
@@ -107,6 +107,27 @@ public class Leetcode {
 		if(tempMax>maxPathSum) maxPathSum=tempMax;
 		return Math.max(root.val, root.val+son);
     }
+	
+	//27、best-time-to-buy-and-sell-stock[数组]
+	/**
+	 * Say you have an array for which the i^(th) element is the price of a given stock on day i.
+	 * If you were only permitted to complete at most one transaction(ie, buy one and sell one share of the stock),
+	 * design an algorithm to find the maximum profit. 
+	 * 
+	 * 很巧妙的方式，记录最小值并每次更新
+	 * 当前最大利润，为当前价格-记录的最小值
+	 * 比较当前利润，并更新最大利润
+	 */
+	public int maxProfit(int[] prices) {
+		if(prices.length==0||prices==null) return 0;
+		int maxProfit=0x80000000;
+		int minPrice=prices[0];
+		for(int i=0;i<prices.length;i++) {
+			minPrice=Math.min(minPrice, prices[i]);
+			maxProfit=Math.max(maxProfit, prices[i]-minPrice);
+		}
+		return maxProfit;
+    }
 
 	//26,  valid-palindrome[递归] 
 	/**
