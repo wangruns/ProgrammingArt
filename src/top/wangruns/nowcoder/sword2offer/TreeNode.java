df899722a155edecc09d740c27fc21920ad157fe
wangruns <1344886192@qq.com>
2018-10-15 12:22:06 +0800

    add nowcoder.sword2offer.TreeNode

---
+++ src/top/wangruns/nowcoder/sword2offer/TreeNode.java
@@ -0,0 +1,17 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * Binary tree
+ *
+ */
+public class TreeNode {
+	int val;
+	TreeNode left=null;
+	TreeNode right=null;
+	
+	TreeNode(int val){
+		this.val=val;
+	}
+
+}
