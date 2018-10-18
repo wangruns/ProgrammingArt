55cfdc191eee667a3c4b727855cb6be2ae0a63a4
wangruns <1344886192@qq.com>
2018-10-18 19:14:37 +0800

    add nowcoder.sword2offer.用两个栈实现队列

---
+++ src/top/wangruns/nowcoder/sword2offer/用两个栈实现队列.java
@@ -0,0 +1,25 @@
+package top.wangruns.nowcoder.sword2offer;
+
+import java.util.Stack;
+
+/**
+ * 
+ * 题目描述
+用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型
+ *
+ */
+public class 用两个栈实现队列 {
+	Stack<Integer> stack1 = new Stack<>();
+	Stack<Integer> stack2 = new Stack<>();
+
+	public void push(int node) {
+		stack1.push(node);
+    }
+    
+    public int pop() {
+    	if(stack2.isEmpty()) {
+    		while(!stack1.isEmpty()) stack2.push(stack1.pop());
+    	}
+    	return stack2.pop();
+    }
+}
