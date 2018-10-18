c8632aa3fef29575c816e3083d8200cc2ea8a4d9
wangruns <1344886192@qq.com>
2018-10-18 22:23:26 +0800

    add nowcoder.sword2offer.斐波那契数列

---
+++ src/top/wangruns/nowcoder/sword2offer/斐波那契数列.java
@@ -0,0 +1,30 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * 题目描述
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+n<=39
+ *
+ */
+public class 斐波那契数列 {
+	
+	//递归方式
+	public int Fibonacci1(int n) {
+		if(n<2) return n;
+		return Fibonacci1(n-1)+Fibonacci1(n-2);
+    }
+	
+	//迭代方式
+	public int Fibonacci2(int n) {
+		if(n<2) return n;
+		int t1=0,t2=1,t3=0;
+		for(int i=2;i<=n;i++) {
+			t3=t1+t2;
+			t1=t2;
+			t2=t3;
+		}
+		return t3;
+	}
+	
+}
