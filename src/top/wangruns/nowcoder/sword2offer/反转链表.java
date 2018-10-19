f5a96555fa59a0ddd4cd98906981efac02e65ccb
wangruns <1344886192@qq.com>
2018-10-19 21:32:40 +0800

    add nowcoder.sword2offer.反转链表

---
+++ src/top/wangruns/nowcoder/sword2offer/反转链表.java
@@ -0,0 +1,22 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * 题目描述
+输入一个链表，反转链表后，输出新链表的表头。
+ *
+ */
+public class 反转链表 {
+
+	public ListNode ReverseList(ListNode head) {
+		ListNode pre=null;
+		while(head!=null) {
+			ListNode temp=head.next;
+			head.next=pre;
+			pre=head;
+			head=temp;
+		}
+		return pre;
+    }
+	
+}
