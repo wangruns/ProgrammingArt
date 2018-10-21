c207a3931c073f5be15f44e2fd5d8be321e1b2c5
wangruns <1344886192@qq.com>
2018-10-21 10:15:06 +0800

    add nowcoder.sword2offer.RandomListNode

---
+++ src/top/wangruns/nowcoder/sword2offer/RandomListNode.java
@@ -0,0 +1,17 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * Randomly linked list
+ *
+ */
+public class RandomListNode {
+	int val;
+	RandomListNode next=null;
+	RandomListNode random=null;
+	
+	RandomListNode(int val) {
+        this.val = val;
+    }
+
+}
