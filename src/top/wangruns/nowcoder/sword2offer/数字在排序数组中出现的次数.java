02cfe8f25741c3f55ccc5f6a5150db6da84e7eb6
wangruns <1344886192@qq.com>
2018-11-02 10:23:29 +0800

    add nowcoder.sword2offer.数字在排序数组中出现的次数

---
+++ src/top/wangruns/nowcoder/sword2offer/数字在排序数组中出现的次数.java
@@ -0,0 +1,39 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * 题目描述
+统计一个数字在排序数组中出现的次数
+ *
+ * 分析
+看看到了排序的数组中查找某一个值很自然的就想到了二分查找，这里可以先查找第一次和最后一次出现的位置
+ */
+public class 数字在排序数组中出现的次数 {
+	
+	public int GetNumberOfK(int [] array , int k) {
+		int l=0,r=array.length-1,firstIndex=1,lastIndex=0;
+		//获取第一次出现的下标
+		while(l<=r) {
+			int mid=(l+r)/2;
+			if(array[mid]<k) l=mid+1;
+			else if(array[mid]==k) {
+				r=mid-1;
+				firstIndex=mid;
+			}
+			else r=mid-1;
+		}
+		l=0;r=array.length-1;
+		//获取第二次出现的下标
+		while(l<=r) {
+			int mid=(l+r)/2;
+			if(array[mid]<k) l=mid+1;
+			else if(array[mid]==k) {
+				l=mid+1;
+				lastIndex=mid;
+			}
+			else r=mid-1;
+		}
+		return lastIndex-firstIndex+1;
+    }
+
+}
