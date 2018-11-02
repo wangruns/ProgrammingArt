a96e7688db88dfd56e68fd0bd61bb4f6941fbf7e
wangruns <1344886192@qq.com>
2018-11-02 10:41:37 +0800

    add nowcoder.sword2offer.二叉树的深度

---
+++ src/top/wangruns/nowcoder/sword2offer/二叉树的深度.java
@@ -0,0 +1,18 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * 题目描述
+输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ *
+ */
+public class 二叉树的深度 {
+	
+	public int TreeDepth(TreeNode root) {
+		if(root==null) return 0;
+        int leftTreeDepth=TreeDepth(root.left);
+        int rightTreeDepth=TreeDepth(root.right);
+        return Math.max(leftTreeDepth, rightTreeDepth)+1;
+    }
+
+}
