ce70c987d3a88485b46c48ffbf47caaaa8751bb3
wangruns <1344886192@qq.com>
2018-11-09 20:25:19 +0800

    add nowcoder.sword2offer.TreeLinkNode

---
+++ src/top/wangruns/nowcoder/sword2offer/TreeLinkNode.java
@@ -0,0 +1,18 @@
+package top.wangruns.nowcoder.sword2offer;
+
+/**
+ * 
+ * Binary tree with pointer to parents
+ *
+ */
+public class TreeLinkNode {
+	int val;
+	TreeLinkNode left;
+	TreeLinkNode right;
+	TreeLinkNode next;
+	
+	TreeLinkNode(int val) {
+        this.val = val;
+    }
+
+}
