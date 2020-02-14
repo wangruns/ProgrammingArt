8d149515249d01b46c4c578f4ba2e05187e1b6b6
wangruns <1344886192@qq.com>
2020-02-14 19:26:17 +0800

    update P1_二维数组中的查找

--- src/top/wangruns/nowcoder/sword2offer/P1_二维数组中的查找.java
+++ src/top/wangruns/nowcoder/sword2offer/P1_二维数组中的查找.java
@@ -7,35 +7,7 @@ package top.wangruns.nowcoder.sword2offer;
 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
  *
  */
-public class 二维数组中的查找 {
-	
-	//暴力法O(N^2)，简单粗暴不推荐
-	public boolean Find1(int target, int [][] array) {
-		int rowLen=array.length;
-		int colLen=array[0].length;
-		for(int i=0;i<rowLen;i++) {
-			for(int j=0;j<colLen;j++) {
-				if(array[i][j]==target) return true;
-			}
-		}
-		return false;
-    }
-	
-	//二分查找O(n*log(n))，因为"有序"所以很容易联想到二分查找，对每一行进行二分查找
-	public boolean Find2(int target, int [][] array) {
-		int rowLen=array.length;
-		int colLen=array[0].length;
-		for(int i=0;i<rowLen;i++) {
-			int l=0,r=colLen-1;
-			while(l<=r) {
-				int middle=(l+r)/2;
-				if(array[i][middle]==target) return true;
-				else if(array[i][middle]>target) r=middle-1;
-				else l=middle+1;
-			}
-		}
-		return false;
-	}
+public class P1_二维数组中的查找 {
 	
 	//利用二维数组(m行n列)从左到右，从上到下都递增的规律O(n+m)=O(n)，从左下角的位置开始，如果向右走则越来越大，如果向上走则越来越小，
 	//利用这个性质，就可以对目标元素进行很好的查找了，即如果a[row][col]<target，col++;如果a[row][col]>target,row--
