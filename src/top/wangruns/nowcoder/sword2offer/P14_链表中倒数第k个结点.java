3a672d526fc54ca0ed3f91674d270cbeae045b27
wangruns <1344886192@qq.com>
2020-02-15 22:28:54 +0800

    update P14_链表中倒数第k个结点

--- src/top/wangruns/nowcoder/sword2offer/P14_链表中倒数第k个结点.java
+++ src/top/wangruns/nowcoder/sword2offer/P14_链表中倒数第k个结点.java
@@ -9,7 +9,7 @@ package top.wangruns.nowcoder.sword2offer;
 第一个结点先跑k，然后第二个结点再跑，这样当第一个结点到达终点的时候，
 第二个结点刚好跑到倒数第k个节点。
  */
-public class 链表中倒数第k个结点 {
+public class P14_链表中倒数第k个结点 {
 	
 	public ListNode FindKthToTail(ListNode head,int k) {
 		ListNode h1=head,h2=head;
