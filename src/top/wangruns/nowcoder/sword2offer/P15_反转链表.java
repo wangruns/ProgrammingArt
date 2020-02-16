ef1ad649ab44aaa779f9ee3e226781ecdee1cc12
wangruns <1344886192@qq.com>
2020-02-16 09:26:30 +0800

    update P15_反转链表

--- src/top/wangruns/nowcoder/sword2offer/P15_反转链表.java
+++ src/top/wangruns/nowcoder/sword2offer/P15_反转链表.java
@@ -6,7 +6,7 @@ package top.wangruns.nowcoder.sword2offer;
 输入一个链表，反转链表后，输出新链表的表头。
  *
  */
-public class 反转链表 {
+public class P15_反转链表 {
 
 	public ListNode ReverseList(ListNode head) {
 		ListNode pre=null;
