323b96cb9ad4b4351c496a33cddc60b397961d72
wangruns <1344886192@qq.com>
2020-03-14 16:22:52 +0800

    update

--- src/top/wangruns/nowcoder/sword2offer/P33_丑数.java
+++ src/top/wangruns/nowcoder/sword2offer/P33_丑数.java
@@ -17,20 +17,22 @@ package top.wangruns.nowcoder.sword2offer;
 不难发现，后面的丑数是前面出现过的一个丑数乘以2，3，5中的一个得来
 故分别用3个标记来记录{2,3,5}已经被前面的哪些数字乘过了，迭代求解
  */
-public class 丑数 {
+public class P33_丑数 {
 	
 	public int GetUglyNumber_Solution(int index) {
-		if(index<=1) return index;
+		if(index==0) return 0;
 		int[]a=new int[index];
 		a[0]=1;
-		int f2=0,f3=0,f5=0;
+		int a2=0,a3=0,a5=0;
 		for(int i=1;i<index;i++) {
-			int t2=a[f2]*2,t3=a[f3]*3,t5=a[f5]*5;
-			int min=Math.min(t2, Math.min(t3, t5));
+			int u2new=a[a2]*2;
+            int u3new=a[a3]*3;
+            int u5new=a[a5]*5;
+			int min=Math.min(u2new, Math.min(u3new, u5new));
 			a[i]=min;
-			if(min==t2) f2++;
-			if(min==t3) f3++;
-			if(min==t5) f5++;
+			if(min==u2new) a2++;
+			if(min==u3new) a3++;
+			if(min==u5new) a5++;
 		}
 		return a[index-1];
 	}
