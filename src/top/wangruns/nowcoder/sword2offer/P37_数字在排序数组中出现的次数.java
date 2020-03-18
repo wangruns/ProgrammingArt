5411bff0862202934878d947a0f915d5e346aaf2
wangruns <1344886192@qq.com>
2020-03-18 14:36:55 +0800

    update

--- src/top/wangruns/nowcoder/sword2offer/P37_数字在排序数组中出现的次数.java
+++ src/top/wangruns/nowcoder/sword2offer/P37_数字在排序数组中出现的次数.java
@@ -8,7 +8,7 @@ package top.wangruns.nowcoder.sword2offer;
  * 分析
 看看到了排序的数组中查找某一个值很自然的就想到了二分查找，这里可以先查找第一次和最后一次出现的位置
  */
-public class 数字在排序数组中出现的次数 {
+public class P37_数字在排序数组中出现的次数 {
 	
 	public int GetNumberOfK(int [] array , int k) {
 		int l=0,r=array.length-1,firstIndex=1,lastIndex=0;
@@ -23,7 +23,7 @@ public class 数字在排序数组中出现的次数 {
 			else r=mid-1;
 		}
 		l=0;r=array.length-1;
-		//获取第二次出现的下标
+		//获取第最后一次出现的下标
 		while(l<=r) {
 			int mid=(l+r)/2;
 			if(array[mid]<k) l=mid+1;
--- src/top/wangruns/nowcoder/sword2offer/P38_二叉树的深度.java
+++ src/top/wangruns/nowcoder/sword2offer/P38_二叉树的深度.java
@@ -6,7 +6,7 @@ package top.wangruns.nowcoder.sword2offer;
 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
  *
  */
-public class 二叉树的深度 {
+public class P38_二叉树的深度 {
 	
 	public int TreeDepth(TreeNode root) {
 		if(root==null) return 0;
